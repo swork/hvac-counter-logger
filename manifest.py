9676b8a766ec61c6ff1664803fdf5f780ddac46c
Steve Work <steve@work.renlabs.com>
2024-01-30 19:54:09 -0800

    Adjust modules requirements for FROZEN_MANIFEST or "mip install" noting external JSON module is bad, broken, unneeded.

--- manifest.py
+++ manifest.py
@@ -1,1 +1,28 @@
+# Modules to freeze into a custom micropython build.
+# We can run on a standard micropython build, but these
+# modules will need to be added to :/lib.
+#
+# There are some builtins that can't be frozen, maybe
+# they're completely in C in upy already? Here's the list:
+#
+# require('gc')
+# require('machine')
+# require('network')
+# require('sys')
+#
+# And this one is just a pothole, lurking to break your wheel.
+# It references re.VERBOSE and other flags, which aren't
+# available or supported in the internal re module
+# (micropython/lib/re1.5). micropython's internal modjson.c
+# does the simple stuff so this isn't needed; I'm guessing
+# it should have been removed when modjson.c was renamed
+# from modujson.c.
+#
+# require('json')
+#
 
+require('aiohttp')
+require('datetime')
+require('os')
+require('time')
+require('unittest')
